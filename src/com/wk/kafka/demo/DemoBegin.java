b34aa1619902408b2c3b45626025fab617c4a29a
wkhuahuo <wkhuahuo@163.com>
2017-06-01 18:09:22 +0800

    新建kafka工程

---
+++ src/com/wk/kafka/demo/DemoBegin.java
@@ -0,0 +1,10 @@
+package com.wk.kafka.demo;
+
+/**
+ * Created by wkhuahuo on 2017/6/1.
+ */
+public class DemoBegin {
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
